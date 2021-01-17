62ff736ecf73399d22d4419c4fd56de08f6597d2
Jere Järvinen <AA5536@student.jamk.fi>
2021-01-17 13:07:15 +0200

    Added github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import github from "../assets/github-brands.svg";
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="github-button">
+            <div
+                className="github-button"
+                onClick={() => window.open("https://github.com/hk279/Solita-assignment-2020-front", "_blank")}
+            >
                 <img className="github-button-icon" src={github} alt="Github logo"></img>
                 <p className="github-button-text">View code on GitHub</p>
             </div>
